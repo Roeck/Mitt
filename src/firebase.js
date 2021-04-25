fdac4396010e05ec48c0c265a9500165cf891813
R B <rons9n@gmail.com>
2021-04-24 22:30:53 -0400

    Firebase setup done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDPQJuVCaxgG2KViwOPhNAiwa6BszlYZL8",
+    authDomain: "mitt-999.firebaseapp.com",
+    projectId: "mitt-999",
+    storageBucket: "mitt-999.appspot.com",
+    messagingSenderId: "349654718949",
+    appId: "1:349654718949:web:e1eac6940a75b0908cf70c",
+    measurementId: "G-S9JTDDHDWT"
+  };
